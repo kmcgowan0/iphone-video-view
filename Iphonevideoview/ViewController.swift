e082f2b4db367b83f5a98384c475c320fa614cef
kmcgowan0 <i7673473@Mac27779.local>
2016-05-18 14:37:57 +0100

    added cocoa pods

---
+++ Iphonevideoview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Iphonevideoview
+//
+//  Created by Abigail Grant (i7643664) on 17/05/2016.
+//  Copyright © 2016 Abigail Grant (i7643664). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Iphonevideoview/Pods-Iphonevideoview-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_IphonevideoviewVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_IphonevideoviewVersionString[];
+
